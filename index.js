b7ad0c69e2a43f961a7260a10793037b911ecd48
Erik Brown <erik.brown@hackreactor.com>
2016-08-14 14:07:50 -0700

    added docs/ to .gitignore createHowTo writes basic yaml file to docs directory

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 var yaml = require('js-yaml');
 var fs = require('fs');
 
-fs.readFile('./howto.yaml', 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(yaml.safeLoad(data));
-	}
-});
+function readHowTo(path) {
+	var path = './docs/' + path;
+	fs.readFile(path, 'utf-8', (err, data) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(yaml.safeLoad(data));
+		}
+	});
+}
+
+
+// takes an objet with title and content properties
+function createHowTo(howto, next) {
+	var yamlFile = yaml.safeDump(howto);
+	var title = howto.title;
+	fs.open('./docs/' + title + '.yaml', 'w', (err, fd) => {
+		fs.write(fd, yamlFile, (err, written, string) => {
+			if (err) {
+				// do something to handle error
+			}
+			console.log('Written: ', written);
+			console.log('String: ', string);
+			fs.close(fd);
+		})
+		
+	})
+}
+
+module.exports = {
+	readHowTo: readHowTo,
+	createHowTo: createHowTo
+}
