fc7d653437180d9ffeef1e787f455949bd52ee03
Erik Brown <erik.brown@hackreactor.com>
2016-08-17 18:20:20 -0700

    set connection to db

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var db = require('../db/config.js');
 var app = express();
 var port = 3000;
 
 app.use(express.static('client'));
 
-app.get('/', function (req, res) {
-  
-});
+
+app.get('/howtos', function (req, res) {});
+app.get('/howtos/:id', function (req, res) {});
+
 
 app.listen(port, function () {
   console.log('Listening on port ' + port);
